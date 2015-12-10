769a11bd0391ab10d6038d807746c361f46821dd
bmacheski <bwm0007@gmail.com>
2015-12-10 12:10:40 -0600

    Refactor post and cookie methods

--- index.js
+++ index.js
@@ -5,11 +5,33 @@ var superagent    = require('superagent-defaults')()
   , cookies       = require('./lib/cookie')
   , cookieJar     = cookies.jar();
 
-superagent.set({
-  'Origin': config.base_url
-});
+superagent.set('Origin', config.base_url);
 
-module.exports = Device;
+var handleCookies = function(res) {
+  function addCookie(cookie) {
+    try {
+      cookieJar.setCookieSync(cookie, config.base_url)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  if (Array.isArray(res.headers['set-cookie'])) {
+    res.headers['set-cookie'].forEach(addCookie)
+  }
+}
+
+var updateDefaults = function() {
+  var cookies = cookieJar.getCookieStringSync(config.base_url);
+  superagent.set('cookie', cookies)
+}
+
+var superagentPost = function(path, data, cb) {
+  superagent
+    .post(path)
+    .send(data)
+    .end(cb)
+}
 
 function Device(apple_id, password, display_name) {
   if (!(this instanceof Device)) return new Device(apple_id, password, display_name);
@@ -23,14 +45,11 @@ function Device(apple_id, password, display_name) {
 Device.prototype.authenticate = function() {
   var self = this;
 
-  superagent
-    .post(config.login_path)
-    .send({
-      'apple_id': this.apple_id,
-      'password': this.password
-    })
-    .end(function(err, res) {
-        if (err) {
+  superagentPost(config.login_path, {
+    'apple_id': this.apple_id,
+    'password': this.password
+  }, function(err, res) {
+      if (err) {
           console.log('Authentication failure. ' + err);
       } else {
           handleCookies(res);
@@ -42,16 +61,16 @@ Device.prototype.authenticate = function() {
 
 Device.prototype.initClient = function() {
   var self = this;
-  this.cookies = cookieJar.getCookieStringSync(config.base_url);
 
-  superagent
-    .post(self.findMeUrl + config.client_init_path)
-    .set('cookie', self.cookies)
-    .end(function(err, res) {
+  updateDefaults();
+
+  superagentPost(self.findMeUrl + config.client_init_path, {
+    'apple_id': this.apple_id,
+    'password': this.password
+  }, function(err, res) {
       if (err) {
-        console.log(err)
-      }
-      else {
+          console.log('Authentication failure. ' + err);
+      } else {
         var content = res.body.content;
         addDevices(content);
 
@@ -86,14 +105,10 @@ Device.prototype.playSound = function(subject) {
   var self = this;
   var subject = subject || 'Find my iPhone';
 
-  superagent
-    .post(self.findMeUrl + config.fmip_sound_path)
-    .set('cookie', self.cookies)
-    .send({
-      'subject': subject,
-      'device': this.dsid
-    })
-    .end(function(err, res) {
+  superagentPost(self.findMeUrl + config.fmip_sound_path, {
+    'apple_id': this.apple_id,
+    'password': this.password
+  }, function(err, res) {
       if (err) {
         console.log(err)
       } else {
@@ -106,16 +121,4 @@ Device.prototype.showDevices = function() {
   return this.devices;
 }
 
-var handleCookies = function(res) {
-  function addCookie(cookie) {
-    try {
-      cookieJar.setCookieSync(cookie, config.base_url)
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  if (Array.isArray(res.headers['set-cookie'])) {
-    res.headers['set-cookie'].forEach(addCookie)
-  }
-}
+module.exports = Device;
